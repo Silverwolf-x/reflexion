730f15f8da259bb7440babc91db48a0dc1c76b62
silverwolf-x <185666132@qq.com>
2024-08-19 00:35:02 +0800

    print运行时使用的ollama模型信息

--- llm.py
+++ llm.py
@@ -5,10 +5,15 @@ from langchain.schema import (
     HumanMessage
 )
 import os
+import subprocess
+import json
 os.environ['OPENAI_API_BASE'] = 'http://localhost:11434/v1'
 os.environ['OPENAI_API_KEY'] = 'MEDAI'
+flag = False
+
 class AnyOpenAILLM:
     def __init__(self, *args, **kwargs):
+        global flag
         # Determine model type from the kwargs
         # model_name = kwargs.get('model_name', 'gpt-3.5-turbo') 
         kwargs['model_name'] = 'llama3'
@@ -18,6 +23,15 @@ class AnyOpenAILLM:
         else:
             self.model = ChatOpenAI(*args, **kwargs)
             self.model_type = 'chat'
+
+        # #### 展示正在使用的ollama模型信息
+        # TODO: 不要在llm.py设定模型，这不优雅
+        if not flag:
+            command = ['curl', 'http://localhost:11434/api/ps']
+            result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            json_data = json.loads(result.stdout)
+            print(f"Model: {json_data['models'][0]['name']}\tParameter: {json_data['models'][0]['details']['parameter_size']}")
+            flag = True
     
     def __call__(self, prompt: str):
         if self.model_type == 'completion':
