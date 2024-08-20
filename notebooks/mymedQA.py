32a29394595f501f0a3498dcff3e06278e06e305
silverwolf-x <185666132@qq.com>
2024-08-20 19:39:03 +0800

    update Q

--- notebooks/mymedQA.py
+++ notebooks/mymedQA.py
@@ -20,15 +20,16 @@ def process_answer(answer):
         if len(answer)==1: return answer[0]
         else: return '+'.join(answer)
     else: print(answer)
-prompt =  "请使用你的已有知识，判断该病人是以下这7个病中的哪一种或多种疾病。脂肪肝，药物性肝损伤，原发性肝癌，酒精性肝病，自身免疫性肝炎，肝囊肿，肝血管瘤。如果病人是多种疾病，用'+'号连接。以下回答都是正确格式：脂肪肝。脂肪肝+药物性肝损伤。药物性肝损伤+脂肪肝+自身免疫性肝炎。"
+# Q =  "请使用你的已有知识，判断该病人是以下这7个病中的哪一种或多种疾病。脂肪肝，药物性肝损伤，原发性肝癌，酒精性肝病，自身免疫性肝炎，肝囊肿，肝血管瘤。如果病人是多种疾病，用'+'号连接。以下回答都是正确格式：Finish[脂肪肝]。Finish[脂肪肝+药物性肝损伤]。Finish[药物性肝损伤+脂肪肝+自身免疫性肝炎]。"
+Q =  "请全程使用中文进行回答。请使用你的已有知识，判断该病人是以下这7个病中的哪一种或多种疾病。脂肪肝，药物性肝损伤，原发性肝癌，酒精性肝病，自身免疫性肝炎，肝囊肿，肝血管瘤。如果病人是多种疾病，用'+'号连接。"
 for ind, row in med.iterrows():
     raw_q = row['question']
     context = raw_q.replace('请判断该病人是以下哪种疾病。病人检查如下：','') # 反整理出病情context
     med.at[ind,'context'] = context 
     # TODO:对齐195，199的evidence的提取
-    med.at[ind,'QAonly'] = prompt+'||'+context
+    med.at[ind,'QAonly'] = Q+'||'+context
+    med.at[ind,'Q'] = Q
     med.at[ind,'process_answer'] = process_answer(row['answer'])
-    # TODO: 如果用context模式，要设计COT引导的sample（看代码）
 
 
 # Define the Reflexion Strategy
